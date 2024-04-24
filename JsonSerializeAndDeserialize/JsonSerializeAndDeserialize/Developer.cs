d975807fd0e59a5edf0a2420d7100f942c731b9d
SiracHuseynov <tu6uvrrlv@code.edu.az>
2024-04-24 05:40:27 +0400

    json add

---
+++ JsonSerializeAndDeserialize/JsonSerializeAndDeserialize/Developer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonSerializeAndDeserialize
+{
+    [Serializable]
+    public class Developer : ISerializable
+    {
+        private static int _id;
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public byte Age { get; set; }   
+        public double Salary { get; set; }
+        public string DevelopmentSkills { get; set; }
+        public int Experience { get; set; }
+
+        public Developer()
+        {
+            _id++;
+            Id = _id;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Name} - {Surname} - {Age} - {Salary} - {DevelopmentSkills} - {Experience}";
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Id", Id, typeof(Developer));
+            info.AddValue("Name", Name, typeof(Developer));
+            info.AddValue("Surname", Surname, typeof(Developer));
+            info.AddValue("Age", Age, typeof(Developer));
+            info.AddValue("Salary", Salary, typeof(Developer));
+            info.AddValue("Development Skills", DevelopmentSkills, typeof(Developer));
+            info.AddValue("Experience", Experience, typeof(Developer));
+
+        }
+    }
+}
---
+++ JsonSerializeAndDeserialize/JsonSerializeAndDeserialize/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+
+namespace JsonSerializeAndDeserialize
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //JSON Serialize
+            Developer developer = new Developer();
+            developer.Name = "Sirac";
+            developer.Surname = "Huseynov";
+            developer.Age = 19;
+            developer.Salary = 15000;
+            developer.DevelopmentSkills = ".NET";
+            developer.Experience = 10;
+            DeveloperToJson(developer);
+
+            //JSON Deserialize
+            Console.WriteLine(JsonToDeveloper());
+        }
+
+        public static void DeveloperToJson(Developer developer)
+        {
+            string path = "C:\\Users\\Sirac Huseynov\\Desktop\\data.json";
+
+            using Stream stream = new FileStream(path, FileMode.Create);
+
+            JsonSerializer.Serialize(stream, developer);
+        }
+
+        public static Developer JsonToDeveloper()
+        {
+            string path = "C:\\Users\\Sirac Huseynov\\Desktop\\data.json";
+
+            using Stream stream = new FileStream(path, FileMode.Open);
+
+            return JsonSerializer.Deserialize<Developer>(stream);
+        }
+    }
+}
